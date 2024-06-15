f62cbe86eba2d410b4d518a3667e16d9ffe3edfe
numToStr <vikasraj1911@gmail.com>
2024-06-15 22:56:45 +0530

    add keebit/coral:numtostr

---
+++ keyboards/keebit/coral/keymaps/numtostr/config.h
@@ -0,0 +1,15 @@
+// Copyright 2023 numToStr (@numToStr)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define MASTER_RIGHT
+
+#define BOOTMAGIC_LITE_ROW 5
+#define BOOTMAGIC_LITE_COLUMN 5
+
+// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM 220
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define QUICK_TAP_TERM 0
