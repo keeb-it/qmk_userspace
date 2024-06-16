5088b8730f77d4baceedd27208ba101d374cecd0
numToStr <vikasraj1911@gmail.com>
2024-06-16 13:18:44 +0530

    hana firmware

---
+++ keyboards/keebit/hana/keymaps/numtostr/layers.h
@@ -0,0 +1,11 @@
+#pragma once
+
+enum hana_layers {
+    /* _M_XYZ = Mac Os, _W_XYZ = Win/Linux */
+    _QWERTY,
+    _COLEMAK,
+    _LOWER,
+    _RAISE,
+    _ADJUST,
+    _GAMING,
+};
---
+++ keyboards/keebit/hana/keymaps/numtostr/rules.mk
@@ -0,0 +1,5 @@
+ENCODER_MAP_ENABLE = yes
+COMBO_ENABLE = yes
+
+OLED_ENABLE = yes
+WPM_ENABLE = yes
